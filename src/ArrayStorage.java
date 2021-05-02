fcfd16397fe776fc287f5bbf1d8d8bfe47a0e44e
ox.sugar <ms.oksana@mail.ru>
2021-05-02 14:46:49 +0300

    Реализация методов save, get, delete, clear, getAll, size в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    List<Resume> list = new ArrayList<>(Arrays.asList(storage));
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Резюме с таким ID уже существует");
+            } else {
+                storage[size() + 1] = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int i;
+        for (i = 0; i < storage.length; i++) {
+            if (!storage[i].uuid.equals(uuid)) {
+                return System.out.println("Index резюме: " + i + "ID резюме: " + storage[i]);
+            }
+        }
+        return System.out.println("Резюме с таким ID " + storage[i] + "не существует");
     }
 
     void delete(String uuid) {
+        list.remove(uuid);
+        storage = list.toArray(new Resume[list.size()]);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int i;
+        int sz = 0;
+        for (i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                sz++;
+            }
+        }
+        return sz;
     }
 }
